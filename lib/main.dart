35d15d385a7dd03695e57deac77256cf030c5237
SonHoang1002 <hoangtrungson07012001@gmail.com>
2023-09-14 19:51:41 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:many_widgets/every_widgets/animated_list.dart';
+import 'package:many_widgets/every_widgets/animated_modal_barrier.dart';
+
+const Color darkBlue = Color(0xFF12202F);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      debugShowCheckedModeBanner: false,
+      title: 'Every Flutter Widget',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Mapp'),
+        ),
+        //! Just change the Widget001 for the Widget you want.
+        body: const Widget013(),
+      ),
+    );
+  }
+}
